8d6fd99b67372801b6e4f6084c01a53657671e21
Andrei Pirvanescu <andrei_parvanescu@yahoo.com>
2026-01-13 00:13:52 +0200

    Spring initalizr application

---
+++ src/main/java/com/example/smart_energy_consumption_and_optimization_platform/SmartEnergyConsumptionAndOptimizationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_energy_consumption_and_optimization_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEnergyConsumptionAndOptimizationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEnergyConsumptionAndOptimizationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_energy_consumption_and_optimization_platform/SmartEnergyConsumptionAndOptimizationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_energy_consumption_and_optimization_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnergyConsumptionAndOptimizationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
